75f9874b57f7db1d549c5dd8f2b6a169f21eac77
Kushuh <kuzanisu@gmail.com>
2021-04-19 19:24:11 +0200

    rewritted test for pharmacy

--- pharmacy.test.js
+++ pharmacy.test.js
@@ -1,9 +1,11 @@
-import { Drug, Pharmacy } from "./pharmacy";
+import Pharmacy from "./pharmacy";
+import { build } from "./drug";
 
 describe("Pharmacy", () => {
   it("should decrease the benefit and expiresIn", () => {
-    expect(new Pharmacy([new Drug("test", 2, 3)]).updateBenefitValue()).toEqual(
-      [new Drug("test", 1, 2)]
-    );
+    const drugs = new Pharmacy([build.Doliprane(2, 3)]).updateBenefitValue();
+    expect(drugs.length).toBe(1);
+    expect(drugs[0].expiresIn).toBe(1);
+    expect(drugs[0].benefit).toBe(2);
   });
 });
