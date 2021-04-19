10119eeef9891155242242712e670df2e900586d
Kushuh <kuzanisu@gmail.com>
2021-04-19 19:18:27 +0200

    rewritten index file

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-import { Drug, Pharmacy } from "./pharmacy";
+import Pharmacy from "./pharmacy";
+import { build } from "./drug";
 import fs from "fs";
 
 // Instantiate available drugs.
 const drugs = [
-  new Drug("Doliprane", 20, 30),
-  new Drug("Herbal Tea", 10, 5),
-  new Drug("Fervex", 5, 40),
-  new Drug("Magic Pill", 15, 40)
+  build.Doliprane(20, 30),
+  build.HerbalTea(10, 5),
+  build.Fervex(5, 40),
+  build.MagicPill(15, 40),
+  build.Dafalgan(20, 50)
 ];
 
 // Instantiate a pharmacy to sell the drugs.
@@ -22,7 +24,7 @@ for (let elapsedDays = 0; elapsedDays < 30; elapsedDays++) {
 
 // Write logs to file.
 /* eslint-disable no-console */
-fs.writeFile("output.txt", log, err => {
+fs.writeFile("output.txt", `[\n\t${log.join(",\n\t")}\n]`, err => {
   if (err) {
     console.log("error");
   } else {
